924eba5da9c238afa0dcb42be97d05b944446cd3
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-04 20:01:26 +0200

    Add light

---
+++ RayTracer/Tracer/Light.swift
@@ -0,0 +1,12 @@
+//
+//  Light.swift
+//  RayTracer
+//
+//  Created by Anatoliy Khramchenko on 04.03.2023.
+//
+
+import Foundation
+
+class Light {
+    static let direction = Vector3D(x: -1, y: -1, z: -1)
+}
