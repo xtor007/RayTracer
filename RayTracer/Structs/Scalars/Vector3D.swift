77a6e4e0089c224142403653ac6256ad42e44e16
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-04 18:40:14 +0200

    Make vector cross product

--- RayTracer/Structs/Scalars/Vector3D.swift
+++ RayTracer/Structs/Scalars/Vector3D.swift
@@ -28,6 +28,14 @@ struct Vector3D: Scalar3D {
     static func * (left: Vector3D, right: Vector3D) -> Float {
         return left.x * right.x + left.y * right.y + left.z * right.z
     }
+    
+    static func * (left: Float, right: Vector3D) -> Vector3D {
+        return Vector3D(
+            x: left * right.x,
+            y: left * right.y,
+            z: left * right.z
+        )
+    }
 
     private(set) var x: Float
     private(set) var y: Float
@@ -35,6 +43,14 @@ struct Vector3D: Scalar3D {
     
     func scalarSquare() -> Float {
         return self * self
+    }
+    
+    func crossProduct(_ vector: Vector3D) -> Vector3D {
+        return Vector3D(
+            x: self.y * vector.z - self.z * vector.y,
+            y: self.z * vector.x - self.x * vector.z,
+            z: self.x * vector.y - self.y * vector.x
+        )
     }
 
 }
