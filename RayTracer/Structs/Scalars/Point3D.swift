e0047d727527d8d1fc0768866103590a4a69caf7
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-04 18:18:30 +0200

    Redefine math operations

--- RayTracer/Structs/Scalars/Point3D.swift
+++ RayTracer/Structs/Scalars/Point3D.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 struct Point3D: Scalar3D {
+    
+    static func + (left: Point3D, right: Scalar3D) -> Point3D {
+        return Point3D(
+            x: left.x + right.x,
+            y: left.y + right.y,
+            z: left.z + right.z
+        )
+    }
+
+    static func - (left: Point3D, right: Scalar3D) -> Point3D {
+        return Point3D(
+            x: left.x - right.x,
+            y: left.y - right.y,
+            z: left.z - right.z
+        )
+    }
 
     private(set) var x: Float
     private(set) var y: Float
--- RayTracer/Structs/Scalars/Vector3D.swift
+++ RayTracer/Structs/Scalars/Vector3D.swift
@@ -8,9 +8,33 @@
 import Foundation
 
 struct Vector3D: Scalar3D {
+    
+    static func + (left: Vector3D, right: Scalar3D) -> Vector3D {
+        return Vector3D(
+            x: left.x + right.x,
+            y: left.y + right.y,
+            z: left.z + right.z
+        )
+    }
+
+    static func - (left: Vector3D, right: Scalar3D) -> Vector3D {
+        return Vector3D(
+            x: left.x - right.x,
+            y: left.y - right.y,
+            z: left.z - right.z
+        )
+    }
+    
+    static func * (left: Vector3D, right: Vector3D) -> Float {
+        return left.x * right.x + left.y * right.y + left.z * right.z
+    }
 
     private(set) var x: Float
     private(set) var y: Float
     private(set) var z: Float
+    
+    func scalarSquare() -> Float {
+        return self * self
+    }
 
 }
