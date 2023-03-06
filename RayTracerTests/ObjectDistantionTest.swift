21fed7f92b7328806103645ee5c88b0cbdaf7b27
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-06 18:02:52 +0200

    Add testing target

---
+++ RayTracerTests/ObjectDistantionTest.swift
@@ -0,0 +1,18 @@
+//
+//  ObjectDistantionTest.swift
+//  RayTracerTests
+//
+//  Created by Anatoliy Khramchenko on 05.03.2023.
+//
+
+import XCTest
+
+final class ObjectDistantionTest: XCTestCase {
+
+    var obj = Sphere(center: Point3D(x: 0, y: 0, z: 0), radius: 5)
+    
+    func testSphere() {
+        
+    }
+
+}
