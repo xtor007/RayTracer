fb1f06751fc9cf3dec69896b6597282b05325c66
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-06 19:20:42 +0200

    Add tests for sphere

--- RayTracer/Structs/Vectors/Ray.swift
+++ RayTracer/Structs/Vectors/Ray.swift
@@ -11,5 +11,10 @@ struct Ray {
 
     let startPoint: Point3D
     let vector: Vector3D
+    
+    init(startPoint: Point3D, vector: Vector3D) {
+        self.startPoint = startPoint
+        self.vector = vector.unitVector
+    }
 
 }
--- RayTracerTests/ObjectDistantionTest.swift
+++ RayTracerTests/ObjectDistantionTest.swift
@@ -9,10 +9,57 @@ import XCTest
 
 final class ObjectDistantionTest: XCTestCase {
 
-    var obj = Sphere(center: Point3D(x: 0, y: 0, z: 0), radius: 5)
+    let camera = Point3D(x: 0, y: 0, z: 0)
+    let sphere = Sphere(center: Point3D(x: 3, y: 5, z: 5), radius: 2)
     
-    func testSphere() {
-        
+    func testSphere1() {
+        let distanceToSphere = sphere.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: 0,
+                y: 5,
+                z: 5
+            )
+        ))
+        XCTAssertNil(distanceToSphere)
+    }
+    
+    func testSphere2() {
+        let distanceToSphere = sphere.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: -3,
+                y: -5,
+                z: -5
+            )
+        ))
+        XCTAssertNil(distanceToSphere)
+    }
+    
+    func testSphere3() {
+        let distanceToSphere = sphere.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: 3,
+                y: 5,
+                z: 5
+            )
+        ))
+        XCTAssertNotNil(distanceToSphere)
+        XCTAssertEqual(distanceToSphere!, sqrt(Float(59)) - 2, accuracy: 0.01)
+    }
+    
+    func testSphere4() {
+        let distanceToSphere = sphere.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: 3,
+                y: 5,
+                z: 3
+            )
+        ))
+        XCTAssertNotNil(distanceToSphere)
+        XCTAssertEqual(distanceToSphere!, sqrt(Float(43)), accuracy: 0.01)
     }
 
 }
