aafd81d9cd46d913ee1907ee0a3fb35d0fe8b867
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-06 20:56:54 +0200

    Add tests for plane

--- RayTracerTests/ObjectDistantionTest.swift
+++ RayTracerTests/ObjectDistantionTest.swift
@@ -11,6 +11,7 @@ final class ObjectDistantionTest: XCTestCase {
 
     let camera = Point3D(x: 0, y: 0, z: 0)
     let sphere = Sphere(center: Point3D(x: 3, y: 5, z: 5), radius: 2)
+    let plane = Plane(point: Point3D(x: 3, y: 0, z: 0), normal: Vector3D(x: 1, y: 0, z: 0))
     
     func testSphere1() {
         let distanceToSphere = sphere.distance(forRay: Ray(
@@ -60,6 +61,43 @@ final class ObjectDistantionTest: XCTestCase {
         ))
         XCTAssertNotNil(distanceToSphere)
         XCTAssertEqual(distanceToSphere!, sqrt(Float(43)), accuracy: 0.01)
+    }
+    
+    func testPlane1() {
+        let distanceToPlane = plane.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: 0,
+                y: 0,
+                z: 1
+            )
+        ))
+        XCTAssertNil(distanceToPlane)
+    }
+    
+    func testPlane2() {
+        let distanceToPlane = plane.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: -1,
+                y: 0,
+                z: 0
+            )
+        ))
+        XCTAssertNil(distanceToPlane)
+    }
+    
+    func testPlane3() {
+        let distanceToPlane = plane.distance(forRay: Ray(
+            startPoint: camera,
+            vector: Vector3D(
+                x: 1,
+                y: 1,
+                z: 1
+            )
+        ))
+        XCTAssertNotNil(distanceToPlane)
+        XCTAssertEqual(distanceToPlane!, 3 * sqrt(Float(3)), accuracy: 0.01)
     }
 
 }
