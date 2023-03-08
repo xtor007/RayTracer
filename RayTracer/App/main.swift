390d7666c159a139dafcac207b97ac884b0c4895
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2023-03-08 19:21:09 +0200

    Final objects

--- RayTracer/App/main.swift
+++ RayTracer/App/main.swift
@@ -8,15 +8,17 @@
 import Foundation
 
 let scene = Scene()
+//scene.addObject(Sphere(center: Point3D(x: 0, y: 0, z: 10), radius: 2))
+//scene.addObject(Sphere(center: Point3D(x: 0, y: 10, z: 50), radius: 10))
+//scene.addObject(Sphere(center: Point3D(x: 8, y: -10, z: 50), radius: 8))
+//scene.addObject(Sphere(center: Point3D(x: -8, y: -10, z: 50), radius: 8))
+
 scene.addObject(Sphere(center: Point3D(x: 0, y: 0, z: 10), radius: 2))
 scene.addObject(Sphere(center: Point3D(x: 0, y: 10, z: 50), radius: 10))
-scene.addObject(Sphere(center: Point3D(x: 8, y: -10, z: 50), radius: 8))
-scene.addObject(Sphere(center: Point3D(x: -8, y: -10, z: 50), radius: 8))
-
-scene.addObject(Disc(center: Point3D(x: 20, y: 10, z: 50),
+scene.addObject(Sphere(center: Point3D(x: -12, y: 1, z: 50), radius: 6))
+scene.addObject(Disc(center: Point3D(x: -20, y: 10, z: 50),
                      normal: Vector3D(x: 60, y: 0, z: 35),
                      radius: 10))
-
 scene.addObject(Plane(point: Point3D(x: 10, y: -10, z: 0),
                       normal: Vector3D(x: 1, y: 0, z: 0)))
 
