0b57b74ea6fa843be5dab6d5296961ede7295266
yasha424 <jakob20022404@gmail.com>
2023-04-10 20:38:01 +0300

    update tests

--- RayTracerTests/ShadowTest.swift
+++ RayTracerTests/ShadowTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 final class ShadowTest: XCTestCase {
-
+    
     func testShadow1() {
         let scene = Scene()
         scene.addObject(Sphere(center: Point3D(x: 1, y: 2, z: 2), radius: 1))
@@ -33,6 +33,35 @@ final class ShadowTest: XCTestCase {
         )
         
         XCTAssertEqual(lighting, 1, accuracy: 0.1)
+    }
+    
+    func testShadow3() {
+        let objects: [Object3D] = [
+            Disc(center: Point3D(x: 1, y: 1, z: 1), normal: Vector3D(x: 0.2, y: 0.8, z: 1), radius: 10),
+            Sphere(center: Point3D(x: 0, y: 0, z: 20), radius: 18),
+            Plane(point: Point3D(x: 0, y: 0, z: 5), normal: Vector3D(x: 0, y: 0, z: 1))
+        ]
+                
+        let doesIntersects = Scene.checkIntersection(withObjects: objects, usingRay: Ray(
+            startPoint: Point3D(x: 0, y: 0, z: 0),
+            vector: Vector3D(x: 0, y: 0, z: 1))
+        )
+        
+        XCTAssertTrue(doesIntersects)
+    }
+
+    func testShadow4() {
+        let objects: [Object3D] = [
+            Sphere(center: Point3D(x: 0, y: 0, z: 20), radius: 10),
+            Sphere(center: Point3D(x: 10, y: 5, z: 10), radius: 1)
+        ]
+                
+        let doesIntersects = Scene.checkIntersection(withObjects: objects, usingRay: Ray(
+            startPoint: Point3D(x: 0, y: 0, z: 0),
+            vector: Vector3D(x: 2, y: 5, z: 1))
+        )
+        
+        XCTAssertFalse(doesIntersects)
     }
 
 }
